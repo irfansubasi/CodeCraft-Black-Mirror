7f5988126b57abfe94a8ea56a1cc390dbc587476
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2025-07-06 16:12:15 +0300

    add fav scene

---
+++ script.js
@@ -0,0 +1,95 @@
+const overlay = document.querySelector('#overlay');
+const modal = document.querySelector('#modal');
+const mainContent = document.querySelector('#mainContent');
+const mainHeader = document.querySelector('.main-header');
+
+let clickCount = 0;
+
+
+const getLocationData = async () => {
+    try {
+        const response = await fetch('https://ipapi.co/json/');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('API Error', error);
+    }
+}
+
+
+
+let locationData = null;
+
+const acceptModal = async () => {
+    
+    const messages = [
+        "We know everything about you.",
+        "Nothing is real.",
+        "Not even you.",
+        "Accept that you are not real.",
+    ]
+    
+    if(clickCount === 0) {
+
+        locationData = await getLocationData();
+        
+        
+        overlay.style.backgroundImage = 'url(./assets/crack1.png)';
+        const messageP = modal.querySelector('p');
+        const messageText = messages[0];
+        messageP.innerHTML = messageText.replace('everything', '<span class="neon-text">everything</span>');
+        
+        if (locationData) {
+            const infoList = document.createElement('ul');
+            infoList.id = 'infoList';
+            infoList.style.listStyle = 'none';
+            infoList.style.display = 'flex';
+            infoList.style.flexDirection = 'column';
+            infoList.style.gap = '10px';
+            
+            const locationItem = document.createElement('li');
+            locationItem.textContent = `Location: ${locationData.city}, ${locationData.country_name}`;
+            infoList.appendChild(locationItem);
+
+            const ipItem = document.createElement('li');
+            ipItem.textContent = `IP: ${locationData.ip}`;
+            infoList.appendChild(ipItem);
+
+            const orgItem = document.createElement('li');
+            orgItem.textContent = `Organization: ${locationData.org}`;
+            infoList.appendChild(orgItem);
+
+            modal.insertBefore(infoList, modal.querySelector('.modal-button-group'));
+        }
+    } else if(clickCount === 1) {
+        infoList.remove();
+        const modalButtonGroup = document.querySelector('.modal-button-group');
+        modalButtonGroup.style.display = 'none';
+        overlay.style.backgroundImage = 'url(./assets/crack2.png)';
+        modal.querySelector('p').innerHTML = messages[1].replace('Nothing', '<span class="neon-text">Nothing</span>');
+
+        setTimeout(() => {
+            const tempText = document.createElement('p');
+            tempText.id = 'tempText';
+            tempText.innerHTML = messages[2].replace('you', '<span class="neon-text">you</span>');
+            modal.insertBefore(tempText, modal.querySelector('.modal-button-group'));
+        }, 2000);
+
+        setTimeout(() => {
+            modalButtonGroup.style.display = 'flex';
+        }, 5000);
+        
+    } else if(clickCount === 2) {
+        tempText.remove();
+        overlay.style.backgroundImage = 'url(./assets/crack3.png)';
+        modal.querySelector('p').innerHTML = messages[3].replace('Accept', '<span class="neon-text">Accept</span>');
+    } else{
+        modal.close();
+        modal.style.display = 'none';
+        overlay.style.display = 'none';
+        mainContent.style.display = 'block';
+        mainHeader.style.display = 'block';
+    }
+
+    clickCount++;
+}
